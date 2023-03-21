c400b4cfe75696370ad6942c13ca6d1c2eeafb50
Jakob Kaya <jakob@macbook-pro-1.home>
2023-03-21 08:28:08 +0100

    added env variable for storyblok access key

--- pages/_app.js
+++ pages/_app.js
@@ -6,20 +6,21 @@ import Page from "../components/Page";
 import Teaser from "../components/Teaser";
 
 const components = {
-  feature: Feature,
-  grid: Grid,
-  teaser: Teaser,
-  page: Page,
+    feature: Feature,
+    grid: Grid,
+    teaser: Teaser,
+    page: Page,
 };
 
 storyblokInit({
-  accessToken: "your-preview-token",
-  use: [apiPlugin],
-  components,
+    accessToken: process.env.sbAccessToken,
+    use: [apiPlugin],
+    components,
 });
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+    return <Component {...pageProps }
+    />;
 }
 
 export default MyApp;
